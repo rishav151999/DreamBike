69d22f5b9569688d8475a478c41ccb6fe7f5e16e
Mukesh Praveen S <Mukesh Praveen S@MUKESH>
2025-09-22 23:15:14 +0530

    BikeGift

--- DreamBike/src/test/java/dreamScooty/DreamHondaScooter.java
+++ DreamBike/src/test/java/dreamScooty/DreamHondaScooter.java
@@ -4,6 +4,7 @@ public class DreamHondaScooter {
 
 	public static void main(String[] args) {
 		System.out.println("BMW C 400 GT");
+		System.out.println("This bike is a gift to rishav girlfriend");
 	}
 
 }
